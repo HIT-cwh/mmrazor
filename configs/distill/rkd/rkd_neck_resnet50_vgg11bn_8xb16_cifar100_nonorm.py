7a159477aba6d4c515b1d9aa49a54627edf3654c
caoweihan <caoweihan@sensetime.com>
2022-03-28 19:20:05 +0800

    add config

--- configs/distill/rkd/rkd_neck_resnet50_vgg11bn_8xb16_cifar100_nonorm.py
+++ configs/distill/rkd/rkd_neck_resnet50_vgg11bn_8xb16_cifar100_nonorm.py
@@ -1,6 +1,6 @@
 _base_ = [
-    '../../_base_/datasets/mmcls/imagenet_bs32.py',
-    '../../_base_/schedules/mmcls/imagenet_bs256.py',
+    '../../_base_/datasets/mmcls/cifar100_bs16.py',
+    '../../_base_/schedules/mmcls/cifar10_bs128.py',
     '../../_base_/mmcls_runtime.py'
 ]
 
@@ -8,15 +8,11 @@ _base_ = [
 student = dict(
     type='mmcls.ImageClassifier',
     backbone=dict(
-        type='ResNet',
-        depth=18,
-        num_stages=4,
-        out_indices=(3, ),
-        style='pytorch'),
+        type='VGG', depth=11, norm_cfg=dict(type='BN')),
     neck=dict(type='GlobalAveragePooling'),
     head=dict(
         type='LinearClsHead',
-        num_classes=1000,
+        num_classes=100,
         in_channels=512,
         loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
         topk=(1, 5),
@@ -27,20 +23,19 @@ teacher = dict(
     type='mmcls.ImageClassifier',
     init_cfg=dict(
         type='Pretrained',
-        checkpoint='/mnt/lustre/caoweihan/checkpoint/cls/resnet34_8xb32_in1k_20210831-f257d4e6.pth'),
+        checkpoint='s3://caoweihan/pretrained/cls/resnet50_b16x8_cifar100_20210528-67b58a1b.pth'),
     backbone=dict(
-        type='ResNet',
-        depth=34,
+        type='ResNet_CIFAR',
+        depth=50,
         num_stages=4,
         out_indices=(3, ),
         style='pytorch'),
     neck=dict(type='GlobalAveragePooling'),
     head=dict(
         type='LinearClsHead',
-        num_classes=1000,
-        in_channels=512,
+        num_classes=100,
+        in_channels=2048,
         loss=dict(type='CrossEntropyLoss', loss_weight=1.0),
-        topk=(1, 5),
     ))
 
 # algorithm setting
@@ -59,7 +54,7 @@ algorithm = dict(
         teacher_norm_eval=True,
         components=[
             dict(
-                student_module='neck.gap',
+                student_module='backbone.features.12',
                 teacher_module='neck.gap',
                 losses=[
                     dict(
@@ -73,3 +68,9 @@ algorithm = dict(
 )
 
 find_unused_parameters = True
+
+optimizer = dict(type='SGD', lr=0.1, momentum=0.9, weight_decay=0.0005)
+lr_config = dict(policy='step', step=[60, 120, 160], gamma=0.2)
+
+data = dict(workers_per_gpu=4)
+evaluation = dict(interval=1, metric='accuracy', save_best='accuracy_top-1')
